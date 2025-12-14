434290a39e60a4256d07ef58d99cd35c6b017c8a
jeraypop <1195668643@qq.com>
2025-12-15 01:03:40 +0800

    90

--- accessibility/src/main/java/com/google/android/accessibility/selecttospeak/SelectToSpeakServiceAbstract.kt
+++ accessibility/src/main/java/com/google/android/accessibility/selecttospeak/SelectToSpeakServiceAbstract.kt
@@ -119,6 +119,9 @@ abstract class SelectToSpeakServiceAbstract : AccessibilityService() {
      */
     override fun onUnbind(intent: Intent?): Boolean {
         runCatching { listeners.forEach { it.onUnbind() } }
+        //手动在设置中关闭 走这里
+        instance = null
+        accessibilityServiceLiveData.value = null
         return super.onUnbind(intent)
     }
     override fun onInterrupt() {
@@ -193,12 +196,17 @@ abstract class SelectToSpeakServiceAbstract : AccessibilityService() {
         var text = getStringOrFallback(Notification.EXTRA_TEXT, bigText)
         val pendingIntent = notification.contentIntent
 
-
         // 尝试判断 解析 MessagingStyle（如果是聊天类型的通知）
-        val messagingStyle = NotificationCompat.MessagingStyle.extractMessagingStyleFromNotification(notification)
+        var messagingStyle: NotificationCompat.MessagingStyle? = null
+        try {
+            messagingStyle = NotificationCompat.MessagingStyle.extractMessagingStyleFromNotification(notification)
+        } catch (_: Exception) { }
         // 如果是 MessagingStyle 类型的通知，解析该类型的标题和内容
         // 获取对话标题（例如联系人名称或群聊名称）
-        val conversationTitle: String = (messagingStyle?.conversationTitle ?: appContext.getString(R.string.notificationtitlenull)).toString()
+        val conversationTitle = messagingStyle?.conversationTitle?.toString()
+            ?.takeIf { it.isNotBlank() }
+            ?: appContext.getString(R.string.notificationtitlenull)
+
         // 获取消息列表
         val messageList = messagingStyle?.messages?:emptyList()
         // 获取所有按时间排序messagingStyle的消息列表 （降序）
@@ -213,7 +221,7 @@ abstract class SelectToSpeakServiceAbstract : AccessibilityService() {
             MessageStyleInfo(
                 timestamp = it.timestamp,  // 时间戳，假设它不会为 null，但可能为 0
                 title = conversationTitle,
-                sender = it.person?.name?.toString() ?: "Unknown",  // 发送者，可能为 null，使用默认值 "Unknown"
+                sender = it.person?.name?.toString()?.takeIf { it.isNotBlank() } ?: "Unknown",  // 发送者，可能为 null，使用默认值 "Unknown"
                 text = it.text?.toString()?.takeIf { it.isNotBlank() }
                     ?: appContext.getString(R.string.notificationcontentnull) // 消息内容，可能为 null 或空白，使用默认值
             )
