d0392f9eaaec5c3b531d5165ab5eb548b8626cfc
jeraypop <1195668643@qq.com>
2026-02-11 16:00:57 +0800

    91

--- app/src/main/java/com/lygttpod/android/auto/notification/LockScreenActivity.kt
+++ app/src/main/java/com/lygttpod/android/auto/notification/LockScreenActivity.kt
@@ -3,6 +3,10 @@ package com.lygttpod.android.auto.notification
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.text.TextUtils
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.google.android.accessibility.ext.acc.XpqAccessibilityUtil
 import com.google.android.accessibility.ext.acc.clickByText
 import com.google.android.accessibility.ext.acc.clickByTextOrDesc
@@ -36,6 +40,7 @@ class LockScreenActivity : BaseLockScreenActivity() {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             context.startActivity(intent)
         }
+        const val dymsg = "定时软件下载地址在群公告,请点击群公告查看哦(本条消息为自动发送)"
         /**
          * 根据索引获取对应的发送列表
          */
@@ -53,9 +58,16 @@ class LockScreenActivity : BaseLockScreenActivity() {
                 GlobalScope.launch {
                     AliveUtils.piSend(pending)
                     sendLog("通知已跳转(如果实测没跳,则是安卓系统偶尔的抽疯)")
-                    delay(3000)
                     inputMsgHint()
                     clickDYSend()
+                    //输入框还有内容,意味着 发送按钮点击失败了
+                    if (!editTextIsEmpty()){
+                        Log.e("查找发送按钮", "输入框不为空" )
+                        //再次点击 发送按钮
+                        clickDYSend()
+                    }else{
+                        Log.e("查找发送按钮", "输入框为空" )
+                    }
                 }
 
             } catch (e: PendingIntent.CanceledException) {
@@ -69,11 +81,14 @@ class LockScreenActivity : BaseLockScreenActivity() {
 
         }
 
-        suspend fun inputMsgHint(msg: String = "定时软件下载地址在群公告,请点击群公告查看哦(本条消息为自动发送)"): Boolean {
-            return delayAction(delayMillis = 1000) {
-                retryCheckTaskWithLog("在发送收藏页自动粘贴发送内容", timeOutMillis = 3000, periodMillis = 1000) {
-                    val editNode = XpqAccessibilityUtil.findEditText{ it.hintText?.toString()?.contains("发送消息") == true }
-                        ?:return@retryCheckTaskWithLog false
+        suspend fun inputMsgHint(msg: String = dymsg): Boolean {
+            return delayAction(delayMillis = 3000) {
+                retryCheckTaskWithLog("自动粘贴发送内容", timeOutMillis = 3000, periodMillis = 1000) {
+                    var editNode = XpqAccessibilityUtil.findEditText{ it.hintText?.toString()?.contains("发送消息") == true || it.hintText?.toString()?.contains("发消息") == true }
+                    if (editNode == null){
+                        editNode = XpqAccessibilityUtil.findEditText{ it.isEditable && it.isFocusable && it.isVisibleToUser }
+                    }
+                    editNode ?: return@retryCheckTaskWithLog false
 
                     if (editNode.text?.toString() == msg) {
                         return@retryCheckTaskWithLog true
@@ -88,9 +103,23 @@ class LockScreenActivity : BaseLockScreenActivity() {
         }
 
         suspend fun clickDYSend(): Boolean {
-            return delayAction(delayMillis = 1000) {
+            return delayAction(delayMillis = 2000) {
                 retryCheckTaskWithLog("点击【发送】按钮", timeOutMillis = 3000, periodMillis = 1000) {
                     accessibilityService?.clickByTextOrDesc("发送",useGesture = true) == true
+                }
+            }
+        }
+
+        suspend fun editTextIsEmpty(msg: String = dymsg): Boolean {
+            return delayAction(delayMillis = 2000) {
+                retryCheckTaskWithLog("判断输入框是否还有内容", timeOutMillis = 3000, periodMillis = 1000) {
+                    var editNode = XpqAccessibilityUtil.findEditText{ it.hintText?.toString()?.contains("发送消息") == true || it.hintText?.toString()?.contains("发消息") == true }
+                    if (editNode == null){
+                        editNode = XpqAccessibilityUtil.findEditText{ it.isEditable && it.isFocusable && it.isVisibleToUser }
+                    }
+                    editNode ?: return@retryCheckTaskWithLog false
+                    Log.e("输入框", "editNode.text: "+editNode.text?.toString() )
+                    !TextUtils.equals(editNode.text,msg)
                 }
             }
         }
