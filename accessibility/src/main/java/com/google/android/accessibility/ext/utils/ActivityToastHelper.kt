b25f50bed11a9c0454f534498225aa4216373478
jeraypop <1195668643@qq.com>
2026-04-25 13:26:17 +0800

    91

---
+++ accessibility/src/main/java/com/google/android/accessibility/ext/utils/ActivityToastHelper.kt
@@ -0,0 +1,42 @@
+package com.google.android.accessibility.ext.utils
+
+import android.widget.Toast
+import com.google.android.accessibility.ext.utils.FloatToastManager.showFloatToast
+import com.google.android.accessibility.ext.utils.KeyguardUnLock.getShowClickIndicator
+import com.google.android.accessibility.ext.utils.LibCtxProvider.Companion.appContext
+
+/**
+ * Company    :
+ * Author     : Lucas     联系WX:780203920
+ * Date       : 2026/4/25  12:35
+ * Description:This is ActivityToastHelper
+ */
+object ActivityToastHelper {
+
+    // Activity -> 描述 映射表
+    private val activityMap = mapOf(
+        // 朋友圈
+        "com.tencent.mm.plugin.sns.ui.SnsTimeLineUI" to "进入朋友圈",
+        "com.tencent.mm.plugin.sns.ui.improve.ImproveSnsTimelineUI" to "进入朋友圈",
+        "com.tencent.mm.plugin.sns.ui.SnsUserUI" to "进入朋友圈",
+
+        // 公众号
+        "com.tencent.mm.plugin.brandservice.ui.timeline.preload.ui.TmplWebViewMMUI" to "进入公众号",
+        "com.tencent.mm.plugin.webview.ui.tools.MMWebViewUI" to "进入公众号"
+    )
+
+    @JvmStatic
+    @JvmOverloads
+    fun showActivityToast(currentActivity: String?, appName: String = "微信",pkgName: String = "com.tencent.mm") {
+        if (currentActivity.isNullOrEmpty()) return
+
+        val description = activityMap[currentActivity]
+        if (!description.isNullOrEmpty()) {
+            if (getShowClickIndicator()){
+                showFloatToast(message = "$appName :$description", packageName = pkgName)
+            }else{
+                AliveUtils.toast(msg = "$appName :$description")
+            }
+        }
+    }
+}
